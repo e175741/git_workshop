c00f7dae848aa4bcddbd7ce146e06c1e87188741
Sho Yoshida <e175758@ie.u-ryukyu.ac.jp>
2018-11-14 15:29:28 +0900

    The first commit Screen transition

---
+++ screen/screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screen
+//
+//  Created by Syou Yoshida on 2018/11/08.
+//  Copyright © 2018 Syou Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
